60ba1ba791134d1cd2cb9428a9aec13b355c1ea5
Isreal Iyaji <isrealiyaji1@gmail.com>
2025-12-14 16:27:13 +0100

    m

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "development") {
 
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 15 * 60 * 1000, 
   max: 100, 
   message: "Too many requests from this IP, please try again later.",
 });
